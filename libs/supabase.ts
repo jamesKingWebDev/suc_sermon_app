c0a29fb277e9a6281e2c2bd0773ec6c881e4dfcb
James King <109388008+jamesKingWebDev@users.noreply.github.com>
2024-04-23 15:46:58 +0100

    Update supabase.ts

--- libs/supabase.ts
+++ libs/supabase.ts
@@ -1,3 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-export const supabase = createClient(process.env.SUPABASE_URL || 'https://wlokbnedtrsgswjabxqg.supabase.co', process.env.SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indsb2tibmVkdHJzZ3N3amFieHFnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTE1OTIwNjMsImV4cCI6MjAyNzE2ODA2M30.10hLsyOMCrbLFdDoMUSGs63kgBdp070nUV0meOYbtI4');
+export const supabase = createClient(
+  //process.env.SUPABASE_URL || 
+  'https://wlokbnedtrsgswjabxqg.supabase.co', 
+  //process.env.SUPABASE_ANON_KEY || 
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indsb2tibmVkdHJzZ3N3amFieHFnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTE1OTIwNjMsImV4cCI6MjAyNzE2ODA2M30.10hLsyOMCrbLFdDoMUSGs63kgBdp070nUV0meOYbtI4');
