f21a9350b29cf58ee600261a5ee9fe6a11ceb6e9
James King <109388008+jamesKingWebDev@users.noreply.github.com>
2024-07-04 13:05:14 +0100

    Update index.ts

--- pages/api/album/index.ts
+++ pages/api/album/index.ts
@@ -6,7 +6,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
   switch (method) {
     case 'GET':
-      const { data } = await supabase.from('album').select(`*, artists (*), songs (*)`).order('id');
+      const { data } = await supabase.from('album').select(`*`).order('id');
       res.status(200).json(data);
       return res.json();
       console.log("Hello World");
